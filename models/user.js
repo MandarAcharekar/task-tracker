3da8c15ac5d8728969757280f712ea1ea7270251
MandarAcharekar <mandarmva8055@gmail.com>
2024-10-13 17:20:14 +0530

    models for task & user

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const moongoose = require('mongoose');
+
+const userSchema = new moongoose.Schema(
+    {
+        username: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+    }, 
+    { timestamps: true }
+);
+
+module.exports = moongoose.model('User', userSchema);

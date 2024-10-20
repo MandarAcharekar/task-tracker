7ef88ff2db5549f20ca3fea79b001c8693d9c4da
MandarAcharekar <mandarmva8055@gmail.com>
2024-10-20 21:50:22 +0530

    Tasks CRUD operations

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -20,10 +20,59 @@ export const createTask = async (req, res) => {
 };
 
 export const getTasks = async (req, res) => {
+    console.log("GET TASKS")
     try{
         const tasks = await Task.find({ assignedTo: req.user.userId });
 
         res.status(200).json({ tasks });
+    }catch(error){
+        res.status(500).json({ message: `Internal Server Error - ${error.message}` });
+    }
+};
+
+export const getTaskById = async (req, res) => {
+    const { id } = req.params;
+    console.log("GET TASK BY ID")
+
+    try{
+        const task = await Task.findById(id);
+        console.log(task)
+        if (!task) return res.status(404).json({ message: `Task not found` });
+
+        res.status(200).json({ task });
+    }catch(error){
+        res.status(500).json({ message: `Internal Server Error - ${error.message}` });
+    }
+};
+
+export const updateTask = async (req, res) => {
+    const { id } = req.params;
+    const { title, description, dueDate, status } = req.body;
+
+    try{
+        const task = await Task.findById(id);
+        if (!task) return res.status(404).json({ message: `Task not found` });
+
+        task.title = title || task.title;
+        task.description = description || task.description;
+        task.dueDate = dueDate || task.dueDate;
+        task.status = status || task.status;
+
+        await task.save();
+        res.status(200).json({ message: 'Task updated', task });
+    }catch(error){
+        res.status(500).json({ message: `Internal Server Error - ${error.message}` });
+    }
+};
+
+export const deleteTask = async (req, res) => {
+    const { id } = req.params;
+    try{
+        const task = await Task.findById(id);
+        if (!task) return res.status(404).json({ message: `Task not found` });
+
+        await task.remove();
+        res.status(200).json({ message: `Task deleted` });
     }catch(error){
         res.status(500).json({ message: `Internal Server Error - ${error.message}` });
     }
--- routes/task.route.js
+++ routes/task.route.js
@@ -1,11 +1,14 @@
 import express from 'express';
 
-import { createTask, getTasks } from "../controllers/task.controller.js";
+import { createTask, getTasks, getTaskById, updateTask, deleteTask } from "../controllers/task.controller.js";
 import { authMiddleware } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
-router.get('/',authMiddleware, getTasks);
 router.post('/',authMiddleware, createTask);
+router.get('/:id',authMiddleware, getTaskById);
+router.put('/:id',authMiddleware, updateTask);
+router.delete('/:id',authMiddleware, deleteTask);
+router.get('/',authMiddleware, getTasks);
 
 export default router;
